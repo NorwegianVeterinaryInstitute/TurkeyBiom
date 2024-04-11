83493943cc787d81606aca5f57c31467f0b16a0d
hkaspersen <hakon.kaspersen@protonmail.com>
2024-04-11 13:19:26 +0200

    corrected the analysis

--- scripts/09_calculate_diversity.R
+++ scripts/09_calculate_diversity.R
@@ -15,6 +15,11 @@ library(microViz)
 library(tibble)
 library(patchwork)
 library(lsr)
+library(pairwiseAdonis)
+
+# Set options ----
+options(scipen = 999)
+set.seed(12345)
 
 # 01. Import data ----
 ## Define paths ----
@@ -91,8 +96,6 @@ group_names <- c(
   "antibiotic" = "P",
   "none" = "None"
 )
-
-options(scipen = 999)
 
 # 02. Alpha diversity ----
 ## Calculate alpha diversity ----
@@ -173,15 +176,11 @@ ggsave(
 )
 
 # 03. Beta diversity ----
-### Calculate beta diversity ----
-beta_ord <- ordinate(
-  physeq = bracken_data,
-  method = "NMDS", 
-  distance = "bray",
-  autotransform = FALSE,
-  k = 2,
-  try = 30
-  )
+### Calculate bray-curtis distance ----
+bray_dists <- phyloseq::distance(
+  bracken_data,
+  method = "bray"
+)
 
 ### Statistical analyses ----
 sample_beta_data <- as(sample_data(bracken_data), "data.frame") %>%
@@ -198,66 +197,41 @@ sample_beta_data <- as(sample_data(bracken_data), "data.frame") %>%
                                    "none"),
                         ordered = TRUE))
 
+#### PERMANOVA ----
 adonis_test <- adonis2(
-  phyloseq::distance(
-    bracken_data,
-    method = "bray"
-  ) ~ group + sex,
+  bray_dists ~ group + sex,
   data = sample_beta_data,
   method = "bray",
   permutations = 9999)
 
+#### Dispersion tests ----
 disp_test_groups <- betadisper(
-    phyloseq::distance(
-      bracken_data,
-      method = "bray"
-    ),
+    bray_dists,
     type = "median",
     group = sample_beta_data$group
   )
 
 disp_test_sex <- betadisper(
-  phyloseq::distance(
-    bracken_data,
-    method = "bray"
-  ),
+  bray_dists,
   type = "median",
   group = sample_beta_data$sex
 )
 
-TukeyHSD(disp_test_groups,
-         conf.level = 0.95)
+#### Pairwise PERMANOVA ----
+pairwise.adonis(
+  bray_dists,
+  sample_beta_data$group
+)
 
-TukeyHSD(disp_test_sex,
-         conf.level = 0.95)
+pairwise.adonis(
+  bray_dists,
+  sample_beta_data$sex
+)
 
-
-### Plot NMDS ----
-#### NMDS dotplot
-beta_plot_data <- as.data.frame(beta_ord$points) %>%
-  rownames_to_column("id") %>%
-  left_join(metadata)
-
-p_nmds_beta <- ggplot(beta_plot_data, aes(MDS1, MDS2, color = group)) +
-  geom_point(size = 3) +
-  labs(title = "A") +
-  scale_color_manual(values = palette_group,
-                     labels = group_names) +
-  theme_bw() +
-  theme(legend.title = element_blank(),
-        panel.grid = element_blank(),
-        legend.position = "none",
-        legend.text = element_text(size = 7)) +
-  xlim(-0.5, 0.53)
-
-
-#### Boxplot of distances
+### Boxplot of distances ----
 boxplot_data <- as.data.frame(
   as.matrix(
-    distance(
-      bracken_data,
-      method = "bray"
-    )
+    bray_dists
   )
 ) %>%
   rownames_to_column("id1") %>%
@@ -267,11 +241,11 @@ boxplot_data <- as.data.frame(
   filter(id1 != id2) %>%
   left_join(metadata[,c("id", "sex", "group")],
             by = c("id1" = "id")) %>%
-  rename("sex1" = sex,
+  dplyr::rename("sex1" = sex,
          "group1" = group) %>%
   left_join(metadata[,c("id", "sex", "group")],
             by = c("id2" = "id")) %>%
-  rename("sex2" = sex,
+  dplyr::rename("sex2" = sex,
          "group2" = group)
 
 p_bray_boxplot <- boxplot_data %>%
@@ -280,6 +254,14 @@ p_bray_boxplot <- boxplot_data %>%
   stat_boxplot(geom = "errorbar", width = 0.4, 
                position = position_dodge(0.8)) +
   geom_boxplot(position = position_dodge(0.8)) +
+  geom_signif(y_position = c(0.62, 0.64),
+              xmin = c(3,2),
+              xmax = c(4,3),
+              annotation = c("*","*"),
+              tip_length = 0.02,
+              size = 0.6,
+              textsize = 4,
+              vjust = 0.5) +
   labs(y = "Bray-Curtis distance",
        title = "B") +
   scale_fill_manual(values = palette_group) +
@@ -289,13 +271,211 @@ p_bray_boxplot <- boxplot_data %>%
         axis.title.x = element_blank(),
         legend.position = "none")
 
-## Plot PCA ----
-pca_data <- bracken_data %>%
+
+
+# 04. Constrained analysis ----
+## Prepare data ----
+ord_data <- bracken_data %>%
+  # select the bacterial fraction of the data
   tax_select("Bacteria", 
              ranks_searched = "Rank1", 
              strict_matches = TRUE) %>%
-  tax_agg("Rank6") %>%
-  ord_calc(method = "PCA") %>%
+  # aggregate to genus-level
+  tax_agg(rank = "Rank6") %>%
+  # transform to compositional
+  tax_transform(trans = "compositional", keep_counts = TRUE) %>%
+  # filter out low-abundant taxa
+  tax_filter(
+    min_sample_abundance = 0.01,
+    tax_level = "Rank6",
+    use_counts = FALSE,
+    prev_detection_threshold = 0
+  ) %>%
+  # revert to counts
+  ps_get(counts = TRUE)
+
+sampledf <- data.frame(sample_data(bracken_data)) %>%
+  mutate(group = case_when(
+    monensin == TRUE & antibiotic == TRUE ~ "monensin_antibiotic",
+    monensin == TRUE & antibiotic == FALSE ~ "monensin",
+    monensin == FALSE & antibiotic == TRUE ~ "antibiotic",
+    monensin == FALSE & antibiotic == FALSE ~ "none"
+  )) %>%
+  mutate(group = factor(group,
+                           levels = c("monensin_antibiotic",
+                                      "antibiotic",
+                                      "monensin",
+                                      "none")),
+         sex = factor(sex)) %>%
+  select(sex, group)
+
+vegan_otu <- function(physeq) {
+  OTU <- otu_table(physeq)
+  if (taxa_are_rows(OTU)) {
+    OTU <- t(OTU)
+  }
+  return(as(OTU, "matrix"))
+}
+
+otutable <- vegan_otu(ord_data)
+
+## Run distance-based constrained analysis ----
+ord_all <- capscale(
+  otutable ~ ., 
+  distance = "bray", 
+  data = sampledf,
+  autotransform = FALSE,
+  sqrt.dist = TRUE
+  )
+
+ord_group <- capscale(
+  otutable ~ group, 
+  distance = "bray", 
+  data = sampledf,
+  autotransform = FALSE,
+  sqrt.dist = TRUE
+)
+
+ord_sex <- capscale(
+  otutable ~ sex, 
+  distance = "bray", 
+  data = sampledf,
+  autotransform = FALSE,
+  sqrt.dist = TRUE
+)
+
+ord_interaction <- capscale(
+  otutable ~ sex * group, 
+  distance = "bray", 
+  data = sampledf,
+  autotransform = FALSE,
+  sqrt.dist = TRUE
+)
+
+## Check significance of models ----
+anova.cca(ord_all)
+anova.cca(ord_group)
+anova.cca(ord_sex)
+anova.cca(ord_interaction)
+
+anova.cca(ord_all, by = "axis")
+anova.cca(ord_all, by = "terms")
+anova.cca(ord_all, by = "margin")
+
+## Check if there are unimportant terms in the model
+spec_rda <- step(ord_all, scope = formula(ord_all), test = "perm")
+summary(spec_rda)
+## No change to model
+
+## Inspect collinearity, exclude variables with a VIF > 20
+vif.cca(ord_all)
+## No variables > 20
+
+## Get R-squared for the whole model
+RsquareAdj(ord_all)
+
+## Check significance of factors ----
+anova.cca(ord_all, by = "onedf", permutations = 999)
+anova.cca(ord_group, by = "onedf", permutations = 999)
+anova.cca(ord_sex, by = "onedf", permutations = 999)
+anova.cca(ord_interaction, by = "onedf", permutations = 999)
+### Group "Antibiotic" is not significant
+### Interaction model not significant for the interaction terms
+
+## Fit variables to model ----
+### Extract the scores from the RDA for plotting
+site.scrs <- as.data.frame(scores(ord_all, display = "sites"))
+site.scrs <- cbind(site.scrs, sex = sampledf$sex)
+site.scrs <- cbind(site.scrs, group = sampledf$group)
+
+### Fit environmental variables to the RDA
+otu_env_fit <- envfit(ord_all, sampledf, permutations = 999)
+
+env.scrs <- as.data.frame(scores(otu_env_fit, display = "factors")) %>%
+  rownames_to_column("env.variables") %>%
+  filter(env.variables != "groupantibiotic") %>%
+  mutate(env.variables = case_when(
+    env.variables == "sexMale" ~ "Male",
+    env.variables == "sexFemale" ~ "Female",
+    env.variables == "groupmonensin_antibiotic" ~ "M+P",
+    env.variables == "groupmonensin" ~ "M",
+    env.variables == "groupnone" ~ "None",
+    TRUE ~ env.variables
+  ))
+
+### Fit taxa to the RDA
+otu_taxa_fit <- envfit(ord_all, otutable, permutations = 999)
+
+spp.scrs <- as.data.frame(scores(otu_taxa_fit, display = "vectors"))
+spp.scrs <- cbind(spp.scrs, Species = rownames(spp.scrs))
+spp.scrs <- cbind(spp.scrs, pval = otu_taxa_fit$vectors$pvals)
+spp.scrs <- cbind(spp.scrs, r = otu_taxa_fit$vectors$r)
+sig.spp.scrs <- subset(spp.scrs, pval <= 0.05) %>%
+  mutate(r = round(r, 3))
+
+write_delim(
+  sig.spp.scrs,
+  here(
+    output_path,
+    "09_envfit_taxa_sign.txt"
+  ),
+  delim = "\t"
+)
+
+sig.spp.scrs <- slice_max(sig.spp.scrs, n = 10, order_by = r)
+
+## Plot the results ----
+p_rda <- ggplot(site.scrs, aes(x = CAP1, y = CAP2))+ 
+  geom_hline(yintercept = 0,
+             color = "grey80") +
+  geom_vline(xintercept = 0,
+             color = "grey80") +
+  geom_point(aes(colour = group,
+                 shape = sex),
+             size = 3) + 
+  stat_ellipse(aes(color = group),
+               type = "t",
+               level = 0.6) +
+  geom_segment(data = sig.spp.scrs, 
+               aes(x = 0, xend = CAP1, y = 0, yend = CAP2), 
+               arrow = arrow(length = unit(0.2, "cm")), 
+               colour = "grey40",
+               alpha = 0.5,
+               lwd = 0.4,
+               inherit.aes = FALSE) +
+  ggrepel::geom_text_repel(data = sig.spp.scrs, 
+            aes(x = CAP1, y = CAP2, label = Species), 
+            size = 4,
+            fontface = "italic",
+            min.segment.length = Inf,
+            max.overlaps = 25) +
+  geom_segment(data = env.scrs,
+               aes(x = 0, xend = CAP1, y = 0, yend = CAP2),
+               arrow = arrow(length = unit(0.25, "cm")),
+               colour = "#377eb8",
+               lwd = 1) +
+  geom_text(data = env.scrs,
+            aes(x = CAP1, y = CAP2, label = env.variables,
+                angle = (180/pi) * atan(CAP2/CAP1),
+                hjust = (1 - 2 * sign(CAP1)) / 3),
+            cex = 4,
+            color = "#377eb8") +
+  labs(title = "A",
+       x = "CAP1 (53.14%)",
+       y = "CAP2 (33.85%)") +
+  scale_color_manual(values = palette_group, labels = group_names) +
+  scale_fill_manual(values = palette_group) +
+  theme_bw() +
+  theme(panel.grid = element_blank(),
+        legend.title = element_blank())
+
+# 05. Abundance of specific taxa ----
+genera_data <- bracken_data %>%
+  tax_transform(trans = "compositional") %>%
+  tax_agg(rank = "Rank6") %>%
+  tax_select(sig.spp.scrs$Species, 
+             ranks_searched = "Rank6", 
+             strict_matches = TRUE) %>%
   ps_mutate(group = case_when(
     monensin == TRUE & antibiotic == TRUE ~ "monensin_antibiotic",
     monensin == TRUE & antibiotic == FALSE ~ "monensin",
@@ -303,119 +483,172 @@ pca_data <- bracken_data %>%
     monensin == FALSE & antibiotic == FALSE ~ "none"
   )) %>%
   ps_mutate(group = factor(group,
-                        levels = c("monensin_antibiotic",
-                                   "antibiotic",
-                                   "monensin",
-                                   "none"),
-                        ordered = TRUE))
+                           levels = c("monensin_antibiotic",
+                                      "antibiotic",
+                                      "monensin",
+                                      "none"),
+                           ordered = TRUE)) %>%
+  psmelt()
 
-p_pca <- pca_data %>%
-  ord_plot( 
-    size = 2.5, 
-    color = "group",
-    plot_taxa = 1:10,
-    plot_samples = TRUE,
-    tax_vec_length = 0.6,
-    auto_caption = NA,
-    tax_lab_style = tax_lab_style(
-      type = "text", 
-      max_angle = 90, 
-      aspect_ratio = 1,
-      size = 2, 
-      fontface = "italic"
-    )
-  ) +
-  theme_classic() +
-  scale_color_manual(values = palette_group,
-                     labels = group_names) +
-  labs(color = NULL,
-       title = "C") +
+
+tests <- lapply(
+  sig.spp.scrs$Species, function(x) {
+    genera_data %>%
+      filter(Rank6 == x) %>%
+      aov(Abundance ~ group, data = .) %>%
+      broom::tidy() %>%
+      mutate(ref = x)
+  }
+) %>%
+  bind_rows() %>%
+  filter(!is.na(p.value)) %>%
+  filter(p.value < 0.05)
+
+stat_bifido <- genera_data %>%
+  filter(Rank6 == "Bifidobacterium") %>%
+  aov(Abundance ~ group, data = .)
+
+stat_megaspaera <- genera_data %>%
+  filter(Rank6 == "Megasphaera") %>%
+  aov(Abundance ~ group, data = .)
+
+stat_megamonas <- genera_data %>%
+  filter(Rank6 == "Megamonas") %>%
+  aov(Abundance ~ group, data = .)
+
+TukeyHSD(stat_bifido)
+TukeyHSD(stat_megaspaera)
+TukeyHSD(stat_megamonas)
+
+## Create plots
+p_bifido <- genera_data %>%
+  filter(Rank6 == "Bifidobacterium") %>%
+  ggplot(aes(group, Abundance, fill = group)) +
+  stat_boxplot(geom = "errorbar", width = 0.5) +
+  geom_boxplot() +
+  geom_signif(y_position = c(0.32, 0.35),
+              xmin = c(1,1),
+              xmax = c(2,4),
+              annotation = c("*","*"),
+              tip_length = 0.02,
+              size = 0.6,
+              textsize = 5) +
+  labs(title = "C",
+       subtitle = "*Bifidobacterium*",
+       y = "Relative abundance") +
+  scale_fill_manual(values = palette_group) +
+  scale_y_continuous(limits = c(0, 0.38)) +
   theme_bw() +
   theme(panel.grid = element_blank(),
-        legend.position = "none") +
-  xlim(-600, 2500)
+        plot.subtitle = ggtext::element_markdown(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.x = element_blank(),
+        legend.position = "none")
 
-p_scree <- bracken_data %>%
-  tax_select("Bacteria", 
-             ranks_searched = "Rank1", 
-             strict_matches = TRUE) %>%
-  tax_agg("Rank6") %>%
-  ord_calc(method = "PCA") %>%
-  ord_get() %>%
-  plot_scree() +
-  scale_x_discrete(limits = paste0("PC", 1:6)) +
-  labs(y = "Eigenvalues",
-       x = "Principal components",
-       title = "D") +
+p_megasphaera <- genera_data %>%
+  filter(Rank6 == "Megasphaera") %>%
+  ggplot(aes(group, Abundance, fill = group)) +
+  stat_boxplot(geom = "errorbar", width = 0.5) +
+  geom_boxplot() +
+  geom_signif(y_position = c(0.07, 0.09),
+              xmin = c(2,3),
+              xmax = c(3,4),
+              annotation = c("*","*"),
+              tip_length = 0.04,
+              size = 0.6,
+              textsize = 5) +
+  labs(subtitle = "*Megasphaera*",
+       y = "Relative abundance") +
+  scale_fill_manual(values = palette_group) +
+  scale_y_continuous(limits = c(0, 0.38)) +
   theme_bw() +
-  theme(panel.grid = element_blank())
+  theme(panel.grid = element_blank(),
+        plot.subtitle = ggtext::element_markdown(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        legend.position = "none")
+
+p_megamonas <- genera_data %>%
+  filter(Rank6 == "Megamonas") %>%
+  ggplot(aes(group, Abundance, fill = group)) +
+  stat_boxplot(geom = "errorbar", width = 0.5) +
+  geom_boxplot() +
+  geom_signif(y_position = c(0.12, 0.14),
+              xmin = c(2,3),
+              xmax = c(3,4),
+              annotation = c("*","*"),
+              tip_length = 0.04,
+              size = 0.6,
+              textsize = 5) +
+  labs(subtitle = "*Megamonas*",
+       y = "Relative abundance") +
+  scale_fill_manual(values = palette_group) +
+  scale_y_continuous(limits = c(0, 0.38)) +
+  theme_bw() +
+  theme(panel.grid = element_blank(),
+        plot.subtitle = ggtext::element_markdown(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        legend.position = "none")
 
 
-p_beta <- p_nmds_beta + p_bray_boxplot + p_pca + p_scree +
-  plot_layout(ncol = 2,
-              widths = c(0.7, 0.3),
-              heights = c(0.4, 0.6))
+rest_plots <- lapply(
+  sig.spp.scrs$Species[-c(1,9,10)],
+  function(x) {
+    genera_data %>%
+      filter(Rank6 == x) %>%
+      ggplot(aes(group, Abundance, fill = group)) +
+      stat_boxplot(geom = "errorbar", width = 0.5) +
+      geom_boxplot() +
+      labs(subtitle = paste0("*", x, "*"),
+           y = "Relative abundance") +
+      scale_fill_manual(values = palette_group) +
+      scale_y_continuous(limits = c(0, 0.38)) +
+      theme_bw() +
+      theme(panel.grid = element_blank(),
+            plot.subtitle = ggtext::element_markdown(),
+            axis.text.x = element_blank(),
+            axis.ticks.x = element_blank(),
+            axis.title.x = element_blank(),
+            legend.position = "none")
+  }
+)
+
+# 06. Merge plots ----
+
+all_plots <- append(rest_plots, list(p_megasphaera, p_megamonas), after = 7)
+all_plots <- append(all_plots, list(p_bifido), after = 0)
+
+
+plot_design <- c(
+  "AAAB
+   AAAB
+   CDEF
+  "
+)
+
+p_beta <- p_rda + 
+  p_bray_boxplot + 
+  p_bifido +
+  p_megasphaera +
+  p_megamonas +
+  guide_area() +
+  plot_layout(design = plot_design,
+              guides = "collect")
 
 ggsave(
   here(
     output_path,
-    "09_beta_div.png"
+    "09_rda_ord.png"
   ),
   p_beta,
   device = "png",
   units = "cm", 
   dpi = 600,
-  height = 16,
-  width = 20
+  height = 25,
+  width = 27
 )
 
-
-# 04. Canonical correspondence analysis ----
-### TODO: Need to run this correctly! ----
-cca_data <- bracken_data %>%
-  tax_select("Bacteria", 
-             ranks_searched = "Rank1", 
-             strict_matches = TRUE) %>%
-  tax_agg(rank = "Rank6") %>%
-  ord_calc(
-    constraints = c("monensin"),
-    method = "CCA"
-  )
-
-anova(cca_data@ord)
-anova(cca_data@ord, by = "axis")
-
-p_cca <- cca_data %>% ord_plot(
-  size = 3,
-  color = "monensin_plot",
-  plot_taxa = 1:20,
-  tax_vec_length = 2.5,
-  auto_caption = NA,
-  tax_lab_style = tax_lab_style(
-    type = "text",
-    max_angle = 90,
-    aspect_ratio = 0.6,
-    size = 2,
-    fontface = "italic"
-  )
-) +
-  theme_bw() +
-  theme(panel.grid = element_blank()) +
-  scale_color_manual(values = palette_monensin) +
-  labs(color = "Monensin") +
-  xlim(-3, 5) +
-  ylim(-5, 6) +
-  coord_fixed(ratio = 0.6)
-
-ggsave(
-  here(
-    output_path,
-    "09_cca.png"
-  ),
-  p_cca,
-  device = "png",
-  units = "cm", 
-  dpi = 600,
-  height = 15,
-  width = 20
-)
